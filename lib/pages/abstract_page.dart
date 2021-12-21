9e1ca906c47fdbccf185318b750daef588b0af33
majusiregar@gmail.com <47182263+bangcadel@users.noreply.github.com>
2021-12-21 13:12:06 +0700

    'create-login-page'

---
+++ lib/pages/abstract_page.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+abstract class Pages extends StatelessWidget {
+  const Pages({Key? key}) : super(key: key);
+}
---
+++ lib/pages/page.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:refactor_template/pages/abstract_page.dart';
+
+part 'login_page.dart';
